cdaef759e28a31e4441e67801f8c037e82b91dc7
Miri Shiwana <nicezki.jp@gmail.com>
2023-07-19 22:33:20 +0900

    Progress Update (Jul 19, 2023)
    + train_status now have new properties total_epoch, acc, history, finished and result
    + add set_training_config to aimane
    + add some logic to provide data in new properties total_epoch, acc, history, finished and result
    + fix inconsistency when training with stop on epoch
    + add route for testing
    + add ssl and now only serve via ssl
    + add script for AIManeUI (finally!!) but still in-progress

--- app/sysmane.py
+++ app/sysmane.py
@@ -35,9 +35,13 @@ class SysMane:
             'stage': "Not running",
             'percentage': -1,
             'epoch': -1,
+            'total_epoch': -1,
             'batch': -1,
             'loss': -1,
-            'acc': -1
+            'acc': -1,
+            'history': {},
+            'finished': False,
+            'result': 'Not ready'
         }
 
         self.log_buffer = []
@@ -213,7 +217,7 @@ class SysMane:
         status_string = json.dumps(status)
         return status_string
     
-    def set_train_status(self, status=None, stage=None, percentage=None, epoch=None, batch=None, loss=None, acc=None):
+    def set_train_status(self, status=None, stage=None, percentage=None, epoch=None, batch=None, loss=None, acc=None, total_epoch=None, finished = None, result=None):
         # Set time  
         self.train_status['time'] = self.get_current_time()
         # Get status from the self.current_status
@@ -231,7 +235,26 @@ class SysMane:
             self.train_status['acc'] = acc
         if percentage is not None:
             self.train_status['percentage'] = percentage
+        if total_epoch is not None:
+            self.train_status['total_epoch'] = total_epoch
+        if finished is not None:
+            self.train_status['finished'] = finished
+        if result is not None:
+            self.train_status['result'] = result
+        
 
+        # If epoch is not None and acc is not None, add the epoch and acc to the history
+        # data will be {epoch: acc}
+        if epoch is not None and acc is not None:
+            #Check if new epoch is more than the last epoch
+            if epoch >= self.train_status['epoch']:
+                self.train_status['history']['e{}'.format(epoch)] = acc
+            else:
+            #Reset the history
+                self.write_status(status='Resetting history because epoch {} is less than the last epoch {}'.format(epoch,self.train_status['epoch']))
+                self.train_status['history'] = {}
+                self.train_status['history']['e{}'.format(epoch)] = acc
+        
         # Convert the status to string
         status_string = json.dumps(self.train_status)
         return status_string
@@ -244,9 +267,6 @@ class SysMane:
 
     # This is the lib not the main function
     # Prevent the program from running when imported
-    
-    #Message: Please run the program from the server.py file.
-    
     if __name__ == "__main__":
         # Print Red Error Message
         print("{}[ERR] SysMane can't be run as a standalone program." .format(Fore.RED))
